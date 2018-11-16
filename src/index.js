6a6dca5531d5e314ddb3db0e77e270df7eb2fbd8
DannyBoyNYC <daniel.deverell@gmail.com>
2018-11-15 20:07:21 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import getEm from './getEm.js';
+import angular from 'angular';
+import ngRoute from 'angular-route';
 
-var elem = document.querySelector('#app');
+const app = angular.module('foodApp', ['ngRoute']);
 
-getEm(elem);
+app.config(function config($locationProvider, $routeProvider){
+  $routeProvider.when('/', {
+    template: `<h1>Home</h1>`
+  })
+  .when('/recipes', {
+    template: '<recipe-list></recipe-list>'
+  })
+  .when('/recipes/:recipeId', {
+    template: '<recipe-detail></recipe-detail>'
+  })
+  $locationProvider.html5Mode(true);
+})
+
+app.component('recipeList', {
+  templateUrl: '/templates/recipes.html',
+  controller: function($scope, $http){
+    $http.get('api/recipes').then( res => {
+      $scope.recipes = res.data;
+    })
+  }
+})
+
+app.component('recipeDetail', {
+  template: '<p>View for {{recipeId}}</p>',
+  controller: function recipeDetailController($scope, $routeParams){
+    $scope.recipeId = $routeParams.recipeId
+  }
+})
